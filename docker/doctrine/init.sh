c248f40218a531492fae36690069b936447db779
Christophe Villeger <cvilleger@users.noreply.github.com>
2018-07-06 13:02:33 +0000

    Build & Test with Travis CI and lightweight containers (#13)
    * Create a lightweight docker-compose.yml and containers, update Travis with Docker, add optional docker-compose.override.yml.dist and remove in test env the old sqlite database to be the default dev one.
    
    * Refactoring Database.php to use Khepin purge&load fixtures feature instead of classic Doctrine to more efficient and add phpunit Travis last build command.
    
    * Remove old docker folder and composer update
    
    * Uncomment phpunit command for Travis
    
    * Update README.md

--- docker/doctrine/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-php /app/bin/console doctrine:database:create
-php /app/bin/console doctrine:schema:create
-php /app/bin/console khepin:yamlfixtures:load 
--- tests/AppBundle/Utils/Database.php
+++ tests/AppBundle/Utils/Database.php
@@ -1,33 +1,14 @@
 <?php
 namespace Tests\AppBundle\Utils;
 
-use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
-use Doctrine\Common\DataFixtures\Loader;
-use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use \Symfony\Bundle\FrameworkBundle\Client;
 
 class Database
 {
-    public static function cleanDb(Client $client)
-    {
-        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
-        $connection = $em->getConnection();
-        $loader = new Loader();
-        $purger = new ORMPurger($em);
-        $purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
-        $executor = new ORMExecutor($em, $purger);
-        $executor->execute($loader->getFixtures());
-    }
-
-    public static function loadFixtures(Client $client)
-    {
-        $client->getContainer()->get('khepin.yaml_loader')->loadFixtures('test');
-    }
-
     public static function prepareDb(Client $client)
     {
-        self::cleanDb($client);
-        self::loadFixtures($client);
+        $client->getContainer()->get('khepin.yaml_loader')->purgeDatabase('orm');
+        $client->getContainer()->get('khepin.yaml_loader')->loadFixtures('test');
     }
 
     public static function getLast(Client $client, $repository)
